23a0cb236dff1159513c83518f41362171b92886
Harold Kimball <spht.lhar@gmail.com>
2016-12-19 21:05:53 +0000

    Added Hirb gem for data presentation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,9 @@
 class ContactsController < ApplicationController
+ 
   def new
     @contact = Contact.new
   end
+  
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
@@ -10,8 +12,10 @@ class ContactsController < ApplicationController
        redirect_to new_contact_path, notice: "Error occured."
     end
   end
+  
   private
     def contact_params
        params.require(:contact).permit(:name, :email, :comments)
     end
+    
 end
