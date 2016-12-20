1725672857d5a9f26d674089bb5904023f58e5ea
Harold Kimball <spht.lhar@gmail.com>
2016-12-20 06:05:48 +0000

    Added Mailer to ContactController path

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,12 +8,19 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Message Sent."
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      ContactMailer.contact_email(name, email, body).deliver
        redirect_to new_contact_path
     else
       flash[:danger] = @contact.errors.full_messages.join(", ")
        redirect_to new_contact_path
     end
+    
+    
   end
+  
   
   private
     def contact_params
